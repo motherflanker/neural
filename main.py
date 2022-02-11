15ca6c41a37ecee9166c6cd1a54fafe6a1db168f
motherflanker <geth.jhjgh@mail.ru>
2022-02-11 19:28:44 +0300

    1

--- main.py
+++ main.py
@@ -2,20 +2,48 @@ import sys
 import matplotlib
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]
-           ]
-biases = [2, 3, 0.5]
-
-output = np.dot(weights, inputs) + biases
-print(output)
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,3)
+
+layer1.forward(X)
+#print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
 
 '''
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
+
+
+
 output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
           inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
           inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
