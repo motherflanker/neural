1918f373d38f499f18ee5696ed28ad2ae6dcd681
motherflanker <geth.jhjgh@mail.ru>
2022-03-06 21:07:38 +0300

    RMSprop(root mean square propagation) Optimizer

--- main.py
+++ main.py
@@ -148,6 +148,30 @@ class AdaGrad_Optimizer:
         self.iterations += 1
 
 
+class RMSprop_Optimizer:
+    def __init__(self, learning_rate=0.022, decay=0., epsilon=1e-7, rho=0.89):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+    def update_params(self, layer):
+        if not hasattr(layer, 'weight_cache'):   #create cache arrays
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        #update cache with current gradient squared
+        layer.weight_cache = self.rho * layer.weight_cache + (1 - self.rho) * layer.dweights ** 2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.dbiases ** 2
+        #regular parameter update and a square root normalization
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+    def post_update_params(self):
+        self.iterations += 1
+
 
 X, y = spiral_data(samples=100, classes=3)
 
@@ -158,7 +182,8 @@ dense2 = Layer_Dense(64, 3)   #there is 3 outputs on the previous level so the n
 loss_activation = Softmax_Activation_CategoricalCrossEntropy_Loss_Combined()
 
 #optimizer = SGD_Optimizer(decay=1e-3, momentum=0.96)
-optimizer = AdaGrad_Optimizer(decay=1e-4)
+#optimizer = AdaGrad_Optimizer(decay=1e-4)
+#optimizer = RMSprop_Optimizer(decay=1e-4)
 
 for epoch in range(10001):
     dense1.forward(X)
