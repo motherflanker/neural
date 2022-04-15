b9fb31ace3066291ab91c8f7de81b2365c5d714f
motherflanker <geth.jhjgh@mail.ru>
2022-04-15 21:27:35 +0300

    Activation_Sigmoid

--- main.py
+++ main.py
@@ -56,7 +56,12 @@ class Activation_ReLU:
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
 
-
+class Activation_Sigmoid:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-inputs))
+    def backward(self, dvalues):
+        self.dinputs = dvalues * (1 - self.output) * self.output
 
 class Activation_Softmax:
     def forward(self, inputs):
